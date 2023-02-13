04cb6af7b635ae9d8a3760e2f5acb553ba95bb5b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-13 15:54:38 +0100

    Added the columns for the table in the db

--- database/migrations/2023_02_13_142440_create_comics_table.php
+++ database/migrations/2023_02_13_142440_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title',100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->float('price',8,2);
+            $table->string('series',100);
+            $table->dateTime('sale_date');
+            $table->string('type',50);
             $table->timestamps();
         });
     }
