4bff4c6054295a615fc18db257e9ed765c6b9026
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-13 17:58:54 +0100

    Modified the visualization of the elements of comics

--- resources/views/Admin/comics/index.blade.php
+++ resources/views/Admin/comics/index.blade.php
@@ -23,14 +23,14 @@
         <tbody>
             @foreach ($comics as $comic)
                 <tr>
-                    <td>{{$comic['id']}}</td>
-                    <td>{{$comic['title']}}</td>
-                    <td>{{$comic['description']}}</td>
-                    <td>{{$comic['thumb']}}</td>
-                    <td>{{$comic['price']}}</td>
-                    <td>{{$comic['sale_date']}}</td>
-                    <td>{{$comic['type']}}</td>
-                    <td><a href="{{route('comics.show' , $comic['id'])}}">Show</a></td>
+                    <td>{{$comic->id}}</td>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->description}}</td>
+                    <td>{{$comic->thumb}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td><a href="{{route('comics.show' , $comic->id)}}">Show</a></td>
                 </tr>
             @endforeach
         </tbody>
--- resources/views/Admin/comics/show.blade.php
+++ resources/views/Admin/comics/show.blade.php
@@ -12,11 +12,11 @@
             <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}" title="{{$comic['series']}}">
         </div>
         <ul>
-            <li>{{$comic['title']}}</li>
-            <li>{{$comic['description']}}</li>
-            <li>{{$comic['price']}} $</li>
-            <li>{{$comic['sale_date']}}</li>
-            <li>{{$comic['type']}}</li>
+            <li>{{$comic->title}}</li>
+            <li>{{$comic->description}}</li>
+            <li>{{$comic->price}}</li>
+            <li>{{$comic->sale_date}}</li>
+            <li>{{$comic->type}}</li>
         </ul>
     </article>
 </body>
