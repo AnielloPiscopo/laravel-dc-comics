f7f511ef28bbd5ee409b37a221724edae107dac7
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-14 15:58:11 +0100

    Added the scss

--- resources/views/Admin/comics/show.blade.php
+++ resources/views/Admin/comics/show.blade.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comic - {{$comic['title']}}</title>
-</head>
-<body>
-    <article class="my_card">
-        <div class="my_img-container">
-            <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}" title="{{$comic['series']}}">
-        </div>
-        <ul>
-            <li>{{$comic->title}}</li>
-            <li>{{$comic->description}}</li>
-            <li>{{$comic->price}}</li>
-            <li>{{$comic->sale_date}}</li>
-            <li>{{$comic->type}}</li>
-        </ul>
-    </article>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Comic - {{$comic['title']}}</title>
+    </head>
+    <body>
+        <article class="my_card">
+            <div class="my_img-container">
+                <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}" title="{{$comic['series']}}">
+            </div>
+            <ul>
+                <li>{{$comic->title}}</li>
+                <li>{{$comic->description}}</li>
+                <li>{{$comic->price}}</li>
+                <li>{{$comic->sale_date}}</li>
+                <li>{{$comic->type}}</li>
+            </ul>
+        </article>
+    </body>
 </html>
