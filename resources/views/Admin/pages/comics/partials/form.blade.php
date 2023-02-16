2b95902afe4cf759c9315c65e8b1e02876714d63
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-16 22:30:20 +0100

    Added the possibility to view to old version of the comics values insert in the form input field area after an error

--- resources/views/Admin/pages/comics/partials/form.blade.php
+++ resources/views/Admin/pages/comics/partials/form.blade.php
@@ -2,23 +2,81 @@
     @csrf
     @method($formMethod)
 
-    @if ($errors->any())
+    @if ($errors->has('title'))
     <div>
         <ul>
-            @foreach ($errors->all() as $error)
+            @foreach ($errors->get('title') as $error)
                 <li>{{$error}}</li>
             @endforeach
         </ul>
     </div>
     @endif
+    <input type="text" name="title" placeholder="Insert the title" value="{{old('title') ?? $comic->title}}">
 
+    @if ($errors->has('description'))
+    <div>
+        <ul>
+            @foreach ($errors->get('description') as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <textarea name="description" cols="30" rows="10" placeholder="Insert the description">{{old('description') ?? $comic->description}}</textarea>
 
-    <input type="text" name="title" placeholder="Insert the title" value="{{$comic->title}}">
-    <textarea name="description" cols="30" rows="10" placeholder="Insert the description">{{$comic->description}}</textarea>
-    <input type="text" name="thumb" placeholder="Insert the thumb of the img" value="{{$comic->thumb}}">
-    <input type="number" name="price" placeholder="Insert the price"  step="0.01" value="{{$comic->price}}" min='0.1' max='999999.99'>
-    <input type="text" name="series" placeholder="Insert the series" value="{{$comic->series}}">
-    <input type="date" name="sale_date" placeholder="Insert the sale date" value="{{$comic->sale_date}}">
-    <input type="text" name="type" placeholder="Insert the type" value="{{$comic->type}}">
+    @if ($errors->has('thumb'))
+    <div>
+        <ul>
+            @foreach ($errors->get('thumb') as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <input type="text" name="thumb" placeholder="Insert the thumb of the img" value="{{old('thumb') ?? $comic->thumb}}">
+
+    @if ($errors->has('price'))
+    <div>
+        <ul>
+            @foreach ($errors->get('price') as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <input type="number" name="price" placeholder="Insert the price"  step="0.01" value="{{old('price') ?? $comic->price}}" min='0.1' max='999999.99'>
+
+    @if ($errors->has('series'))
+    <div>
+        <ul>
+            @foreach ($errors->get('series') as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <input type="text" name="series" placeholder="Insert the series" value="{{old('series') ?? $comic->series}}">
+
+    @if ($errors->has('sale_date'))
+    <div>
+        <ul>
+            @foreach ($errors->get('sale_date') as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <input type="date" name="sale_date" placeholder="Insert the sale date" value="{{old('sale_date') ?? $comic->sale_date}}">
+
+    @if ($errors->has('type'))
+    <div>
+        <ul>
+            @foreach ($errors->get('type') as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <input type="text" name="type" placeholder="Insert the type" value="{{old('type') ?? $comic->type}}">
     <button type="submit">Click</button>
 </form>
