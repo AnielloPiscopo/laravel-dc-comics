afa65f895a8b7444d09dd68cfb75991c1a2dc78b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-16 16:10:25 +0100

    Modified the visualization of the sale_date values by creating a specific migration and added the possibility to digit decimal numbers in the specific inputs and also added a range to these ones

--- resources/views/Admin/pages/comics/partials/form.blade.php
+++ resources/views/Admin/pages/comics/partials/form.blade.php
@@ -4,7 +4,7 @@
     <input type="text" name="title" placeholder="Insert the title" value="{{$comic->title}}">
     <textarea name="description" cols="30" rows="10" placeholder="Insert the description">{{$comic->description}}</textarea>
     <input type="text" name="thumb" placeholder="Insert the thumb of the img" value="{{$comic->thumb}}">
-    <input type="number" name="price" placeholder="Insert the price" value="{{$comic->price}}">
+    <input type="number" name="price" placeholder="Insert the price"  step="0.01" value="{{$comic->price}}" min='0.1' max='999999.99'>
     <input type="text" name="series" placeholder="Insert the series" value="{{$comic->series}}">
     <input type="text" name="sale_date" placeholder="Insert the sale date" value="{{$comic->sale_date}}">
     <input type="text" name="type" placeholder="Insert the type" value="{{$comic->type}}">
